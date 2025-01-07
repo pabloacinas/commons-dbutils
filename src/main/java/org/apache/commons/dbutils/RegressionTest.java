e75d432478e848f8d33b337ae547731823c5fe7f
Alejandro Medina <AlejandroMedinaCode@gmail.com>
2025-01-08 00:37:22 +0100

    Ultimos cambios

--- src/main/java/org/apache/commons/dbutils/RegressionTest.java
+++ src/main/java/org/apache/commons/dbutils/RegressionTest.java
@@ -15,11 +15,3 @@
  * limitations under the License.
  */
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ RegressionTest0.class })
-public class RegressionTest {
-}
-
