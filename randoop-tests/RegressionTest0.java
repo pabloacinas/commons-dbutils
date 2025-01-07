63e1e6ff93eba0cad3f1266bf97cbfe500947d60
Alejandro Medina <AlejandroMedinaCode@gmail.com>
2025-01-07 19:12:15 +0100

    Randoop arreglado

--- randoop-tests/RegressionTest0.java
+++ randoop-tests/RegressionTest0.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -8,12 +25,137 @@ public class RegressionTest0 {
     public static boolean debug = false;
 
     @Test
-    public void test1() throws Throwable {
+    public void test01() throws Throwable {
         if (debug)
-            System.out.format("%n%s%n", "RegressionTest0.test1");
+            System.out.format("%n%s%n", "RegressionTest0.test01");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator1 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0);
+        // The following exception was thrown during execution in test generation
+        try {
+            boolean boolean2 = resultSetIterator1.hasNext();
+            org.junit.Assert.fail("Expected exception of type java.lang.NullPointerException; message: Cannot invoke \"java.sql.ResultSet.isLast()\" because \"this.resultSet\" is null");
+        } catch (java.lang.NullPointerException e) {
+            // Expected exception.
+        }
+    }
+
+    @Test
+    public void test02() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test02");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator1 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0);
+        java.lang.Class<?> wildcardClass2 = resultSetIterator1.getClass();
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test03() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test03");
+        java.sql.ResultSet resultSet0 = null;
+        java.lang.Iterable<java.lang.Object[]> objArrayIterable1 = org.apache.commons.dbutils.ResultSetIterator.iterable(resultSet0);
+        java.lang.Class<?> wildcardClass2 = objArrayIterable1.getClass();
+        org.junit.Assert.assertNotNull(objArrayIterable1);
+        org.junit.Assert.assertNotNull(wildcardClass2);
+    }
+
+    @Test
+    public void test04() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test04");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.RowProcessor rowProcessor1 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator2 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0, rowProcessor1);
+        // The following exception was thrown during execution in test generation
+        try {
+            java.lang.Object[] objArray3 = resultSetIterator2.next();
+            org.junit.Assert.fail("Expected exception of type java.lang.NullPointerException; message: Cannot invoke \"java.sql.ResultSet.next()\" because \"this.resultSet\" is null");
+        } catch (java.lang.NullPointerException e) {
+            // Expected exception.
+        }
+    }
+
+    @Test
+    public void test05() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test05");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.RowProcessor rowProcessor1 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator2 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0, rowProcessor1);
+        // The following exception was thrown during execution in test generation
+        try {
+            resultSetIterator2.remove();
+            org.junit.Assert.fail("Expected exception of type java.lang.NullPointerException; message: Cannot invoke \"java.sql.ResultSet.deleteRow()\" because \"this.resultSet\" is null");
+        } catch (java.lang.NullPointerException e) {
+            // Expected exception.
+        }
+    }
+
+    @Test
+    public void test06() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test06");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator1 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0);
+        // The following exception was thrown during execution in test generation
+        try {
+            java.lang.Object[] objArray2 = resultSetIterator1.next();
+            org.junit.Assert.fail("Expected exception of type java.lang.NullPointerException; message: Cannot invoke \"java.sql.ResultSet.next()\" because \"this.resultSet\" is null");
+        } catch (java.lang.NullPointerException e) {
+            // Expected exception.
+        }
+    }
+
+    @Test
+    public void test07() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test07");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator1 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0);
+        // The following exception was thrown during execution in test generation
+        try {
+            resultSetIterator1.remove();
+            org.junit.Assert.fail("Expected exception of type java.lang.NullPointerException; message: Cannot invoke \"java.sql.ResultSet.deleteRow()\" because \"this.resultSet\" is null");
+        } catch (java.lang.NullPointerException e) {
+            // Expected exception.
+        }
+    }
+
+    @Test
+    public void test08() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test08");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.RowProcessor rowProcessor1 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator2 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0, rowProcessor1);
+        // The following exception was thrown during execution in test generation
+        try {
+            boolean boolean3 = resultSetIterator2.hasNext();
+            org.junit.Assert.fail("Expected exception of type java.lang.NullPointerException; message: Cannot invoke \"java.sql.ResultSet.isLast()\" because \"this.resultSet\" is null");
+        } catch (java.lang.NullPointerException e) {
+            // Expected exception.
+        }
+    }
+
+    @Test
+    public void test09() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test09");
         java.lang.Object obj0 = new java.lang.Object();
         java.lang.Class<?> wildcardClass1 = obj0.getClass();
         org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test10() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test10");
+        java.sql.ResultSet resultSet0 = null;
+        org.apache.commons.dbutils.RowProcessor rowProcessor1 = null;
+        org.apache.commons.dbutils.ResultSetIterator resultSetIterator2 = new org.apache.commons.dbutils.ResultSetIterator(resultSet0, rowProcessor1);
+        java.lang.Class<?> wildcardClass3 = resultSetIterator2.getClass();
+        org.junit.Assert.assertNotNull(wildcardClass3);
     }
 }
 
